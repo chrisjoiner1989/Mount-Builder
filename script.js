08de83f3f55c085cb4038a1edded22d2ae8b2443
Christopher Joiner <cjoiner9656@gmail.com>
2025-07-12 14:30:04 -0400

    Updated HTML and added Fetch Api

---
+++ script.js
@@ -0,0 +1,7 @@
+// bible api config
+const BIBLE_API_BASE_URL = "https://bible-api.com";
+
+// DOM Elements
+const scriptureSearchInput = document.getElementById("scripture-search");
+const searchBtn = document.querySelector(".search-btn");
+const searchResults = document.getElementById("search-results");
